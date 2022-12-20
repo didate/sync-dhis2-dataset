f5b33253063ab853746c3529d80c29bd29fee39b
Diallo Mamadou Lamarana <lyve.diallo@gmail.com>
2022-12-20 19:03:42 +0000

    archive to dhis2

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ const { dhis2source, dhis2dest } = require('./dhis2');
 const fs = require('fs');
 const csv = require('csv-parser');
 const moment = require('moment');  
-const cron = require("node-cron");
 const config = require('config');
+const { exit } = require('process');
 
 const dataset = config.get("dataset");
-
-
 
 
 const run = async () => {
 
     
-    const periods = getLastFewsWeek(2);
+    const periods = [
+        { year: 2015, start: 1, end: 12 },
+        { year: 2016, start: 1, end: 12 },
+        { year: 2017, start: 1, end: 12 },
+        { year: 2018, start: 1, end: 12 },
+        { year: 2019, start: 1, end: 12 },
+        { year: 2020, start: 1, end: 12 },
+        { year: 2021, start: 1, end: 12 },
+    ]
     const orgunits = [];
 
     fs.createReadStream('./orgunit.csv').pipe(csv()).on('data', (row) => {
@@ -22,97 +28,71 @@ const run = async () => {
         console.log('Starting Sync .....');
 
         for (let i = 0; i < periods.length; i++) {
-                const currentWeek = periods[i];
+            const period = periods[i];
+            let debut = period.start    
+            while (debut <= period.end) {
+                const month = `${debut}`.padStart(2, '0');
+                const currentPeriod = `${period.year}${month}`;
+                for (let index = 0; index < orgunits.length; index++) {
+                    const orgunit = orgunits[index];
+                    console.log(`====================================================`);
+                    console.log(`Current organisation unit : ${orgunit.uid}`);
+                    console.log(`Current period : ${currentPeriod}`);
 
-            for (let index = 0; index < orgunits.length; index++) {
-                const orgunit = orgunits[index];
-                console.log(`====================================================`);
-                console.log(`Current organisation unit : ${orgunit.uid}`);
-                console.log(`Current period : ${currentWeek}`);
-
-                try {
-                    console.log("Get Dataset values from dhis2 mamou .....")
-                    let dataSetValues = await getDataSetValuesFromSource(orgunit.uid, currentWeek);
-                    if (dataSetValues && dataSetValues.dataValues && dataSetValues.dataValues.length > 0) {
-                        //delete dataSetValues['completeDate']
-                        console.log("Send Dataset values to dhis2 Surveillance .....")
-                        postDataSetValuesToDestination(dataSetValues, orgunit.uid, currentWeek);
-
-                        console.log("Get completeness from dhis 2 mamou .....")
-                        const completeness = await getDataSetCompletenessFromSource(orgunit.uid, currentWeek); 
-
-                        if (completeness && completeness.data ) {
-                           console.log("Post completeness to dhis 2 Surveillance .....")
-
-                            postDataSetCompletenessToDestination(completeness.data);
+                    try {
+                        console.log("Get Dataset values from dhis2 source .....")
+                        let dataSetValues = await getDataSetValuesFromSource(orgunit.uid, currentPeriod);
+                        if (dataSetValues && dataSetValues.dataValues && dataSetValues.dataValues.length > 0) {
+                            delete dataSetValues['orgUnit']
+                            delete dataSetValues['dataSet']
+                            delete dataSetValues['period']
+                            console.log("Send Dataset values to dhis2 destination .....")
+                            await postDataSetValuesToDestination(dataSetValues, orgunit.uid, currentPeriod);
                         }
-
+                    } catch (error) {
+                        console.log(error);
+                        logNOTOK(orgunit.uid, currentPeriod);
                     }
-                } catch (error) {
-                   
-                    logNOTOK(orgunit.uid, currentWeek);
                 }
+                logOK(currentPeriod)
+                debut = debut +1;
             }
-            logOK(currentWeek)
         }
 
         console.log('End Sync .....');
     });
 }
 
-
-const getLastFewsWeek = (numberOfWeek=4) =>{
-    index =1 ;
-    const weeks = [];
-    while(index <= numberOfWeek){
-        const period = moment().add(-(index++), 'w');
-        weeks.push(`${period.year()}W${period.isoWeek()}`);
-    }
-    return weeks;
-}
-
-
-const getDataSetValuesFromSource = async (orgunit , week) => {
-    const datavaluesfromdhis = await dhis2source.get(`/dataValueSets?orgUnit=${orgunit}&dataSet=${dataset}&period=${week}`).catch((error) => {
-        logNOTOK(orgunit, week);
+const getDataSetValuesFromSource = async (orgunit , currentPeriod) => {
+    const url = `/dataValueSets?orgUnit=${orgunit}&dataSet=${dataset}&period=${currentPeriod}`;
+    const datavaluesfromdhis = await dhis2source.get(url).catch((error) => {
+        console.log(error);
+        logNOTOK(orgunit, currentPeriod);
         return null;
     });
 
     return datavaluesfromdhis ? datavaluesfromdhis.data : null;
 }
 
-const postDataSetValuesToDestination = async (dataSetValues , orgunit, week) => {
+const postDataSetValuesToDestination = async (dataSetValues , orgunit, currentPeriod) => {
 
     await dhis2dest.post('/dataValueSets?importStrategy=CREATE_AND_UPDATE', JSON.stringify(dataSetValues)).catch((error) => {
-       logNOTOK(orgunit, week)
+        console.log(error)
+        logNOTOK(orgunit, currentPeriod)
     })
 }
 
-const getDataSetCompletenessFromSource =async (orgunit , week) => {
-    return await dhis2source.get(`/completeDataSetRegistrations?orgUnit=${orgunit}&dataSet=${dataset}&period=${week}`).catch((error) => {
-        return null;
-    });
-}
 
-const postDataSetCompletenessToDestination = async (completeness) => {
-    await dhis2dest.post('/completeDataSetRegistrations?importStrategy=CREATE_AND_UPDATE', JSON.stringify(completeness)).catch((error) => {
-        console.log(error);
-    })
-}
-
-const logOK = (week) =>{
+const logOK = (currentPeriod) =>{
     const date = new Date();
-    fs.writeFile(`./logs/logs_${date.getFullYear()}${date.getMonth()}${date.getDate()}_OK.log`, `${week}\n`, { flag: 'a+' }, err => { })
+    fs.writeFile(`./logs/logs_${date.getFullYear()}${date.getMonth()}${date.getDate()}_OK.log`, `${currentPeriod}\n`, { flag: 'a+' }, err => { })
 }
 
-const logNOTOK = (orgunit , week) =>{
+const logNOTOK = (orgunit , currentPeriod) =>{
     const date = new Date();
-    fs.writeFile(`./logs/logs_${date.getFullYear()}${date.getMonth()}${date.getDate()}_not_OK.log`, `${orgunit} , ${week} \n`, { flag: 'a+' }, err => { });
-    console.log(error);
+    fs.writeFile(`./logs/logs_${date.getFullYear()}${date.getMonth()}${date.getDate()}_not_OK.log`, `${orgunit} , ${currentPeriod} \n`, { flag: 'a+' }, err => { });
+    process.exit(-1)
 }
 
 
 run();
-
-
-// cron.schedule("0 0 18 * * *", async () => await run());
